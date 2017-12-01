054eebc85053efaf59bbdde0e92ffbe8d94224f5
Joel Barron <jbarron_web@hotmail.com>
2017-11-30 19:33:23 -0600

    beta 1

--- dist/server/config/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var secret = exports.secret = process.env.SECRET || 'miclavesecreta';
-var mongoUrl = exports.mongoUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/question-overflow';
-var port = exports.port = process.env.PORT || 3000;
